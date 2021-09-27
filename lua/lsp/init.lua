7ef6364dfc076bca3f23802c811f2127b75185ce
kiyan <yazdani.kiyan@protonmail.com>
2021-09-27 22:45:50 +0200

    chore: fix diagnostics display and add underline

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -166,9 +166,12 @@ function M.setup()
   vim.lsp.handlers['textDocument/references'] = require'lsp.callbacks.references'.references_cb
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-      underline = false,
+      underline = true,
       signs = false,
-      virtual_text = true,
+      virtual_text = {
+        spacing = 2,
+        prefix = ''
+      },
     }
   )
 
